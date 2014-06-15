5b2b32817ac761c5ceff2e95f9812a5dc49d07ad
Jonathan Mukai-Heidt <johnnymukai@gmail.com>
2014-06-14 22:52:24 -0400

    Move shared examples own dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../../validator.rb', __FILE__)
-require File.expand_path('../shared_examples_for_pieces.rb', __FILE__)
+Dir[File.dirname(__FILE__) + '/shared_examples/*.rb'].each { |file| require file }
 
 include CV
 
